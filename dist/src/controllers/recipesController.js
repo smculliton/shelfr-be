1bff367ffdcf6c02c36728a07b3c86d2a1f6bb84
Sean Culliton <smculliton@gmail.com>
2023-04-17 13:56:19 -0600

    Test update dist

---
+++ dist/src/controllers/recipesController.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const getRecipes = (req, res) => {
+    res.send('Hello, this is would be a recipe');
+};
+exports.default = { getRecipes };
